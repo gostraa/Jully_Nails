245986ab2e2b8cc407873f1c9c6e4c17653d2373
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-12-21 22:17:00 +0200

    add translation, and variables

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,11 @@
 import TransitionPanel from "components/TransitionPanel/TransitionPanel.jsx";
-import "./App.styled.js";
-import { useTranslation } from "react-i18next";
 import Hero from "components/Hero/Hero.jsx";
 
+import "./App.styled.js";
+
 function App() {
-  const { t } = useTranslation();
   return (
     <>
-      <div>{t("main")}</div>
       <TransitionPanel />
       <Hero />
     </>
