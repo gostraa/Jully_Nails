1f9016c5d0d4d0f2890f58e01ac46203d77b9438
Mariia <mcherkashina77@gmail.com>
2023-12-24 17:44:05 +0100

    test

--- src/constants/manikureData.js
+++ src/constants/manikureData.js
@@ -1,15 +1,15 @@
 // manikureImg
 
-import img1 from "../images/manikure/1.jpg";
-import img2 from "../images/manikure/2.jpg";
-import img3 from "../images/manikure/3.jpg";
-import img4 from "../images/manikure/4.jpg";
+// import img1 from "../images/manikure/1.jpg";
+// import img2 from "../images/manikure/2.jpg";
+// import img3 from "../images/manikure/3.jpg";
+// import img4 from "../images/manikure/4.jpg";
 
-import img5 from "../images/manikure/5.jpg";
-import img6 from "../images/manikure/6.jpg";
-import img7 from "../images/manikure/7.jpg";
-import img8 from "../images/manikure/8.jpg";
-import img9 from "../images/manikure/9.jpg";
+// import img5 from "../images/manikure/5.jpg";
+// import img6 from "../images/manikure/6.jpg";
+// import img7 from "../images/manikure/7.jpg";
+// import img8 from "../images/manikure/8.jpg";
+// import img9 from "../images/manikure/9.jpg";
 
 // manikureVariants
 
@@ -38,15 +38,12 @@ import eckig2 from "../images/forms/Eckig/2.jpg";
 import eckig3 from "../images/forms/Eckig/3.jpg";
 
 export const manikureImg = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
+  oval1,
+  oval2,
+  oval3,
+  mandelformig1,
+  mandelformig2,
+  mandelformig3,
 ];
 
 export const manikureVariants = [
