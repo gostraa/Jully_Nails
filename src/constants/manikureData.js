cf1745d37a56d78ce7300a8dfde07b15dbccd26f
Mariia <mcherkashina77@gmail.com>
2023-12-24 12:29:00 +0100

    added new photos

--- src/constants/manikureData.js
+++ src/constants/manikureData.js
@@ -1,5 +1,22 @@
 import img1 from "../images/manikure/1.jpg";
 import img2 from "../images/manikure/2.jpg";
 import img3 from "../images/manikure/3.jpg";
+import img4 from "../images/manikure/4.jpg";
 
-export const manikureImg = [img1, img2, img3];
+import img5 from "../images/manikure/5.jpg";
+import img6 from "../images/manikure/6.jpg";
+import img7 from "../images/manikure/7.jpg";
+import img8 from "../images/manikure/8.jpg";
+import img9 from "../images/manikure/9.jpg";
+
+export const manikureImg = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+];
